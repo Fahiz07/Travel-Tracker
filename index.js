c3029a9096591f97f5a7355d87ee36191e8124ff
Mohammed Fahiz <mohammedfahiz07@gmail.com>
2024-07-13 17:39:28 +0530

    update database_url

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 import express from "express";
 import bodyParser from "body-parser";
-import pg from "pg";
+import pkg from 'pg';
 import dotenv from 'dotenv';
 
 const app = express();
 const port = 3000;
+const { Pool } = pkg;
 
 dotenv.config();
 
-const db = new pg.Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, 
+  },
 });
-
-db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
